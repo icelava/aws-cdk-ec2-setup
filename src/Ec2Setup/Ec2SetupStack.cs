d4c03f734471d0a79ba980aa646328b7aa09d909
icelava <icelava@gmail.com>
2021-07-12 15:26:54 +0800

    Initial commit

---
+++ src/Ec2Setup/Ec2SetupStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace Ec2Setup
+{
+    public class Ec2SetupStack : Stack
+    {
+        internal Ec2SetupStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
