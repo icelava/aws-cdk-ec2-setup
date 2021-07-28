30fc413aaa8158acceffb2b7054ada35f6aac592
icelava <icelava@gmail.com>
2021-07-28 16:22:23 +0800

    NACL for public ELB subnet.

--- src/Ec2Setup/Ec2SetupStack.cs
+++ src/Ec2Setup/Ec2SetupStack.cs
@@ -9,6 +9,9 @@ namespace Ec2Setup
         private string vpcCidr = "10.255.248.0/21";
         private string internetCidr = "0.0.0.0/0";
 
+        private string publicElbSubnetName = "cdk_ec2_elb_pub";
+        private string privateWebSubnetName = "cdk_ec2_web_priv";
+
         internal Ec2SetupStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             this.EstablishNetwork();
@@ -24,12 +27,14 @@ namespace Ec2Setup
                 NatGateways = 0, // Do not require NAT gateways.
 
                 SubnetConfiguration = new [] {
-                    new SubnetConfiguration { CidrMask = 28, SubnetType = SubnetType.PUBLIC, Name = "cdk_ec2_elb_pub" },
-                    new SubnetConfiguration { CidrMask = 26, SubnetType = SubnetType.ISOLATED, Name = "cdk_ec2_web_priv" }
+                    new SubnetConfiguration { CidrMask = 28, SubnetType = SubnetType.PUBLIC, Name =  this.publicElbSubnetName},
+                    new SubnetConfiguration { CidrMask = 26, SubnetType = SubnetType.ISOLATED, Name =  this.privateWebSubnetName}
                 }
             });
 
             this.CustomiseRouteTable();
+            this.TightenNacls();
+
         }
 
         private void CustomiseRouteTable()
@@ -76,14 +81,73 @@ namespace Ec2Setup
             }
         }
 
-                private void ConfigurePublic()
+        private void TightenNacls()
         {
+            var elbSubnets = this.vpc.SelectSubnets(new SubnetSelection {SubnetGroupName = this.publicElbSubnetName});
+            var webSubnets = this.vpc.SelectSubnets(new SubnetSelection {SubnetGroupName = this.privateWebSubnetName});
 
-        }
+            var elbNaclName = "public-web-elb";
+            var elbNacl = new NetworkAcl(this.vpc, elbNaclName,
+            new NetworkAclProps
+            {
+                NetworkAclName = elbNaclName,
+                Vpc = this.vpc,
+                SubnetSelection = new SubnetSelection {SubnetGroupName = this.publicElbSubnetName}
+            });
 
-        private void ConfigurePrivate()
-        {
+            var ephemeralPortRange = AclTraffic.TcpPortRange(1024, 65535);
+            // Allow return HTTP response from private web subnets.
+            var responseRuleNumber = 50;
+            foreach (var subnet in webSubnets.Subnets)
+            {
+                elbNacl.AddEntry("Response_HTTP_" + subnet.Node.Id, new CommonNetworkAclEntryOptions
+                {
+                    RuleNumber = responseRuleNumber,
+                    Direction = TrafficDirection.INGRESS,
+                    Cidr = AclCidr.Ipv4(subnet.Ipv4CidrBlock),
+                    Traffic = ephemeralPortRange,
+                    RuleAction = Action.ALLOW
 
+                });
+                responseRuleNumber++;
+            }
+
+            // Allow HTTP request from Internet.
+            elbNacl.AddEntry("Request_Internet_HTTP", new CommonNetworkAclEntryOptions
+            {
+                RuleNumber = 100,
+                Direction = TrafficDirection.INGRESS,
+                Cidr = AclCidr.AnyIpv4(),
+                Traffic = AclTraffic.TcpPort(80),
+                RuleAction = Action.ALLOW
+
+            });
+
+            // Allow HTTP request to private web subnets.
+            var requestRuleNumber = 50;
+            foreach (var subnet in webSubnets.Subnets)
+            {
+                elbNacl.AddEntry("Request_HTTP_" + subnet.Node.Id, new CommonNetworkAclEntryOptions
+                {
+                    RuleNumber = requestRuleNumber,
+                    Direction = TrafficDirection.EGRESS,
+                    Cidr = AclCidr.Ipv4(subnet.Ipv4CidrBlock),
+                    Traffic = AclTraffic.TcpPort(80),
+                    RuleAction = Action.ALLOW
+                });
+                requestRuleNumber++;
+            }
+
+            // Allow HTTP response to Internet.
+            elbNacl.AddEntry("Response_Internet_HTTP", new CommonNetworkAclEntryOptions
+            {
+                RuleNumber = 100,
+                Direction = TrafficDirection.EGRESS,
+                Cidr = AclCidr.AnyIpv4(),
+                Traffic = ephemeralPortRange,
+                RuleAction = Action.ALLOW
+
+            });
         }
 
         private void EstablishEC2()
